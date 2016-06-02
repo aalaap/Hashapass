d38b86c7c026db0748b9029243a4ece3d2ef7d15
aalaap <aalaap@gmail.com>
2016-06-02 14:04:36 +0530

    PSR-1 and PSR-2 compliant, PHP CodeSniffer verified!

--- example.php
+++ example.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * An example script to demonstrate the use of the Hashapass package
+ *
+ * PHP version 5
+ *
+ * @category File
+ * @package  Hashapass
+ * @author   Aalaap Ghag <aalaap@gmail.com>
+ * @license  http://www.wtfpl.net/txt/copying WTFPL
+ * @link     https://github.com/aalaap/Hashapass
+ */ 
+
 require __DIR__ . '/vendor/autoload.php';
 
 use Aalaap\Hashapass\Hashapass;
 
 $hashapass = new Hashapass();
 
-echo '<p>' . 'Default length: ' . $hashapass->generate('master', 'parameter') . '</p>' . PHP_EOL;
-echo '<p>' . 'Custom length: ' . $hashapass->generate('master', 'parameter', 16) . '</p>' . PHP_EOL;
-
-
+echo '<p>' . 'Default length: ' . $hashapass->generate('master', 'parameter') .
+    '</p>' . PHP_EOL;
+echo '<p>' . 'Custom length: ' . $hashapass->generate(
+    'master', 'parameter',
+    16
+) . '</p>' . PHP_EOL;
--- src/Aalaap/Hashapass/Hashapass.php
+++ src/Aalaap/Hashapass/Hashapass.php
@@ -1,22 +1,53 @@
-<?php namespace Aalaap\Hashapass;
+<?php
 
-/*
- * Hashapass
+/**
+ * Hashapass class file
  *
- * A password generator based on HMAC-SHA1 and Base64. The passwords
- * generated are identical to the Hashapass.com website. This generator
- * also allows you to specify a custom size for the generated passwords.
- * 
- * by Aalaap Ghag <aalaap@gmail.com>
+ * PHP version 5
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
- 
+ * @category Class
+ * @package  Hashapass
+ * @author   Aalaap Ghag <aalaap@gmail.com>
+ * @license  http://www.wtfpl.net/txt/copying WTFPL
+ * @link     https://github.com/aalaap/Hashapass
+ */ 
+
+namespace Aalaap\Hashapass;
+
+/**
+ * Hashapass class
+ *
+ * The class holding the root Hashapass class definition
+ *
+ * @category Class
+ * @package  Hashapass
+ * @author   Aalaap Ghag <aalaap@gmail.com>
+ * @license  http://www.wtfpl.net/txt/copying WTFPL
+ * @link     https://github.com/aalaap/Hashapass
+ */ 
 class Hashapass
 {
+    /**
+     * Returns a password hashed using Base64 and HMAC-SHA1.
+     *
+     * @param string $master_password The master password.
+     * @param string $parameter       The variable parameter.
+     * @param int    $length          The length of the password. Default is 8,
+     *                                maximum is 40.
+     *       
+     * @return string
+     */
     public function generate($master_password, $parameter, $length = 8)
     {
-        return substr(base64_encode(hash_hmac('sha1', $parameter, $master_password, TRUE)), 0, $length);
-    }
-}
+        return substr(
+            base64_encode(
+                hash_hmac(
+                    'sha1',
+                    $parameter,
+                    $master_password,
+                    true
+                )
+            ), 0, $length
+        );
+    }//end generate()
+}//end class
